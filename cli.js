1ddc862a8a1d0cd49c3de5db545c7e71f68e1473
Arnelle Balane <arnellebalane@gmail.com>
2018-08-02 22:52:53 +0800

    feat(cli): define inquirer prompts for icon properties

--- cli.js
+++ cli.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-console.log('hello world');
+const inquirer = require('inquirer');
+const icons = require('@mdi/svg/meta.json');
+
+inquirer.prompt([{
+    type: 'input',
+    name: 'iconSize',
+    message: 'Icon size (px):',
+    default: 24
+}, {
+    type: 'input',
+    name: 'iconPadding',
+    message: 'Icon padding (px):',
+    default: 0
+}, {
+    type: 'input',
+    name: 'foregroundColor',
+    message: 'Foreground color:',
+    default: '#333'
+}, {
+    type: 'input',
+    name: 'backgroundColor',
+    message: 'Background color:',
+    default: 'none',
+    filter: value => value === 'none' ? 'transparent' : value
+}]).then(console.log);
